336818377c808f0bfe53400c1e3ca24b8cc64f6d
gang.rao <happyhaoyun@qq.com>
2018-07-20 17:27:10 +0800

    Mod: sigin自动签到程序 1. 实现获取时间，节假日检查 2. 实现将时间检查分开模块实现
    还需要完成：
    1. 定时调用时间检查（具体时间检查）
    2. 自动签到或签退

---
+++ sigin/check_time.py
@@ -0,0 +1,84 @@
+# gettime.py
+# -*- coding: utf-8 -*-
+
+# import urllib
+import requests
+import json
+import re
+import os
+import datetime
+import time
+
+# 通过web接口获取准确的时间
+
+
+# 苏宁获取时间接口 {"sysTime1":"20180720161634","sysTime2":"2018-07-20 16:16:34"}
+url_sn = "http://quan.suning.com/getSysTime.do"
+# qq获取日间接口 2018-07-20 16:17:04
+url_qq = "http://cgi.im.qq.com/cgi-bin/cgi_svrtime"
+# 阿里获取时间的域名 这里返回的是毫秒级时间戳 {"api":"mtop.common.getTimestamp","v":"*","ret":["SUCCESS::接口调用成功"],"data":{"t":"1532074649625"}}
+url_tb = "http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp"
+
+
+# 节假日判断接口
+# http://api.goseek.cn/
+# http://tool.bitefu.net/jiari/
+# 检查是否为节假日 {"code":10000,"data":2} 工作日对应结果为 0, 休息日对应结果为 1, 节假日对应的结果为 2
+url_check_holiday = "http://api.goseek.cn/Tools/holiday?date="
+
+# 通过指定域名url_sn获取当前时间的时间戳
+def get_time_by_sn():
+    try:
+        ret = requests.get(url_sn)
+        print(ret.text)
+    except:
+        print("打开失败" + ret.status_code)
+        return []
+    if 200 != ret.status_code:
+        return []
+
+    print("ret.text: ", ret.text)
+    # 将json字符串转成字典
+    dict = json.loads(ret.text)
+    sysTime = dict['sysTime1']
+    date = sysTime[0:8]
+    time = sysTime[8:]
+    return date, time
+
+
+class check_time:
+    # constructor 初始化获取时间
+    def __init__(self):
+        self.date, self.time = get_time_by_sn()
+
+    # member function 获取今天日期
+    def get_today_date(self):
+        return self.date
+
+    # member function 获取当前时间
+    def get_cur_time(self):
+        return self.time
+
+    def is_holiday(self):
+        url = url_check_holiday + self.date
+        try:
+            ret = requests.get(url)
+        except:
+            print("打开失败" + ret.status_code)
+            return []
+        if 200 != ret.status_code:
+            return []
+
+        # 将json字符串转成字典
+        dict = json.loads(ret.text)
+        ret = dict['data']
+        return ret
+
+
+# if __name__ == '__main__':
+#     print(url_tb)
+#     t = CheckTime()
+#     print(t.get_today_date())
+#     print(t.get_cur_time())
+#     ret = t.is_holiday()
+#     print("工作日对应结果为 0, 休息日对应结果为 1, 节假日对应的结果为 2;返回结果:" + str(ret))
--- sigin/sigin.py
+++ sigin/sigin.py
@@ -1,71 +1,11 @@
 #SyncLocaltime.py
 # -*- coding: utf-8 -*-
 
-# import urllib
-import requests
-import json
-import re
-import os
-import datetime
-import time
-# import win32serviceutil
-# import win32service
-# import win32event
-# import win32evtlogutil
+from check_time import check_time
 
-# 阿里获取时间的域名 这里返回的是毫秒级时间戳
-url = "http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp"
-
-# 苏宁获取时间接口
-# http://quan.suning.com/getSysTime.do
-# qq获取日间接口
-# http://cgi.im.qq.com/cgi-bin/cgi_svrtime
-
-# 通过指定域名获取当前时间的时间戳
-def get_information(url):
-    try:
-        ret = requests.get(url) 
-        print(ret.text)
-    except :
-        print("打开失败" + ret.status_code)
-        return [] 
-    if 200 != ret.status_code:
-        return [] 
-
-    print ("ret.text: ", ret.text)
-    # 将json字符串转成字典
-    dict = json.loads(ret.text)
-    timemap = dict['data']['t']
-    return timemap
-
-def get_time():
-    ans = [] 
-    while True :
-        ch = get_information( url );
-        print(ch)
-        if len(ch) == 0 :
-            print ("30秒后将重新连接")
-            time.sleep( 30 ) 
-        else :
-            break; 
-        print(ch)
-        
-        timearr = time.localtime(int(ch))
-        otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timearr)
-        print (otherStyleTime)
-
-    for i in range(1 , len( ch )-1 ):
-        ss = re.search( r'\d' , ch[i] ).start()
-        needed = ch[i][ss : len( ch[i] ) ]
-        ans.append( int(needed) )
-    os.system("date %d-%d-%d" % ( ans[0] , ans[1] , ans[2]) )
-    os.system("time %d:%d:%d.0" % ( ans[4] , ans[5] ,ans[6]) )
-    date = str(ans[0])
-    print(date)
-
-# 节假日判断接口
-# http://api.goseek.cn/
-# http://tool.bitefu.net/jiari/
-if __name__=='__main__':
-    print(url)
-    get_time()
+if __name__ == '__main__':
+    t = check_time()
+    print(t.get_today_date())
+    print(t.get_cur_time())
+    ret = t.is_holiday()
+    print("工作日对应结果为 0, 休息日对应结果为 1, 节假日对应的结果为 2;返回结果:" + str(ret))
