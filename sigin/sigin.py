a5aa8800464f4d192093ed3265ec04bb7f330235
ray <happyhaoyun@qq.com>
2018-08-22 15:09:30 +0800

    Mod:分离main

--- sigin/sigin.py
+++ sigin/sigin.py
@@ -2,10 +2,18 @@
 # -*- coding: utf-8 -*-
 
 from check_time import check_time
+from log import Log
 
-if __name__ == '__main__':
+
+def main():
     t = check_time()
     print(t.get_today_date())
     print(t.get_cur_time())
     ret = t.is_holiday()
     print("工作日对应结果为 0, 休息日对应结果为 1, 节假日对应的结果为 2;返回结果:" + str(ret))
+
+
+
+
+if __name__ == '__main__':
+    main()
